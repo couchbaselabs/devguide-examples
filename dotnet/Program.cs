4f89adff966c6eef5956ce4fa129897c4a620d20
branor <branor@gmail.com>
2016-01-26 21:43:55 +0200

    Add .NET connection examples

---
+++ dotnet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevGuide
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => AllExamples());
+
+            Console.ReadLine();
+        }
+
+        private static async void AllExamples()
+        {
+            await new ConnectionBase().ExecuteAsync();
+            await new ConnectionConfig().ExecuteAsync();
+        }
+    }
+}
