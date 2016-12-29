e37b0529cb82b2845028abcb8ef80c1cb702d3e7
Igor Vulfson <ivulfson@users.noreply.github.com>
2016-12-29 11:50:10 -0800

    Update connecting.go

--- go/connecting.go
+++ go/connecting.go
@@ -13,12 +13,12 @@ func main() {
 	// Connect to Cluster
 	cluster, err := gocb.Connect("couchbase://127.0.0.1")
 	if err != nil {
-		fmt.Println("ERRROR CONNECTING TO CLUSTER:", err)
+		fmt.Println("ERROR CONNECTING TO CLUSTER:", err)
 	}
 	// Open Bucket
 	bucket, err = cluster.OpenBucket("travel-sample", "")
 	if err != nil {
-		fmt.Println("ERRROR OPENING BUCKET:", err)
+		fmt.Println("ERROR OPENING BUCKET:", err)
 	}
 	fmt.Println("Example Successful - Exiting")
 }
