4b06abd321f12f932976d6b48feb835028f37323
Igor Vulfson <ivulfson@users.noreply.github.com>
2016-12-29 11:52:11 -0800

    Update expiration.go

--- go/expiration.go
+++ go/expiration.go
@@ -14,12 +14,12 @@ func main() {
 	// Connect to Cluster
 	cluster, err := gocb.Connect("couchbase://127.0.0.1")
 	if err != nil {
-		fmt.Println("ERRROR CONNECTING TO CLUSTER:", err)
+		fmt.Println("ERROR CONNECTING TO CLUSTER:", err)
 	}
 	// Open Bucket
 	bucket, err = cluster.OpenBucket("travel-sample", "")
 	if err != nil {
-		fmt.Println("ERRROR OPENING BUCKET:", err)
+		fmt.Println("ERROR OPENING BUCKET:", err)
 	}
 
 	// Create a document and assign an expiration of 2 seconds
@@ -27,14 +27,14 @@ func main() {
 	val := "Expiration Test Value"
 	_, err = bucket.Upsert(key, &val, 2)
 	if err != nil {
-		fmt.Println("ERRROR CREATING DOCUMENT:", err)
+		fmt.Println("ERROR CREATING DOCUMENT:", err)
 	}
 
 	// Retrieve Value Right Away
 	var retValue interface{}
 	_, err = bucket.Get(key, &retValue)
 	if err != nil {
-		fmt.Println("ERRROR RETURNING DOCUMENT:", err)
+		fmt.Println("ERROR RETURNING DOCUMENT:", err)
 	}
 	fmt.Println("Document Not Yet Expired:", retValue)
 
@@ -51,19 +51,19 @@ func main() {
 	//  touch after the document is created of 2 seconds
 	_, err = bucket.Upsert(key, &val, 2)
 	if err != nil {
-		fmt.Println("ERRROR CREATING DOCUMENT:", err)
+		fmt.Println("ERROR CREATING DOCUMENT:", err)
 	}
 
 	// Add an expiry
 	_, err = bucket.Touch(key, 0, 2)
 	if err != nil {
-		fmt.Println("ERRROR TOUCHING DOCUMENT:", err)
+		fmt.Println("ERROR TOUCHING DOCUMENT:", err)
 	}
 
 	// Retrieve Document
 	_, err = bucket.Get(key, &retValue)
 	if err != nil {
-		fmt.Println("ERRROR RETURNING DOCUMENT:", err)
+		fmt.Println("ERROR RETURNING DOCUMENT:", err)
 	}
 	fmt.Println("Document Not Yet Expired:", retValue)
 
