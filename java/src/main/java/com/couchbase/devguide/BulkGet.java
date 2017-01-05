5dde10286eaeed680f5f09c7ab00c1c2fd38c70c
Michael Nitschinger <michael@nitschinger.at>
2017-01-05 16:45:53 +0100

    Update BulkGet.java

--- java/src/main/java/com/couchbase/devguide/BulkGet.java
+++ java/src/main/java/com/couchbase/devguide/BulkGet.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import javax.sound.midi.Soundbank;
-
 import com.couchbase.client.java.document.JsonDocument;
 import com.couchbase.client.java.document.JsonLongDocument;
 import com.couchbase.client.java.document.json.JsonObject;
