8bc1b14c9ddda1500d129fc33f186efb3b9d92b9
Sergey Avseyev <sergey.avseyev@gmail.com>
2017-03-09 10:04:20 +0200

    Update connecting-ssl.php
    Clarify SSL in PHP

--- php/connecting-ssl.php
+++ php/connecting-ssl.php
@@ -6,7 +6,10 @@
  *
  * $ curl http://localhost:8091/pools/default/certificate > /tmp/couchbase-ssl-certificate.pem
  *
- * At the moment SSL support does not work if php-curl module loaded
+ * In spite of libcouchbase linked with SSL itself, when using in PHP context, module php_openssl
+ * have to be loaded before php_couchbase, or it might cause segfaults if other SSL-extensions also
+ * loaded. For example, php_curl + php_couchbase without php_openssl loaded will trigger segfault
+ * on php_curl unloading.
  */
 $cluster = new \Couchbase\Cluster('couchbases://localhost?certpath=/tmp/couchbase-ssl-certificate.pem');
 $bucket = $cluster->openBucket('default');
