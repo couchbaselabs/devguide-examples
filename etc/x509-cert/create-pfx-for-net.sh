b1970694bedffda329832414908a68c9d068f4b2
Jaekwon Park <jaekwon.park@couchbase.com>
2018-05-22 15:00:43 -0700

    PFX file should include root, intemediate certificate

--- etc/x509-cert/create-pfx-for-net.sh
+++ etc/x509-cert/create-pfx-for-net.sh
@@ -13,6 +13,6 @@ cd ${TOPDIR}
 mkdir -p ${PFX_DIR}
 cd ${PFX_DIR}
 
-openssl pkcs12 -export -inkey ../${CLIENT_DIR}/${CLIENT}.key -in ../${CLIENT_DIR}/chain.pem -out client.pfx
+openssl pkcs12 -export -inkey ../${ROOT_DIR}/${ROOT_CA}.key -inkey ../${INT_DIR}/${INTERMEDIATE}.key -inkey ../${CLIENT_DIR}/${CLIENT}.key -in ../${CLIENT_DIR}/chain.pem -out client.pfx
 
 #On windows, double click on pfx file and Install pfx file on Local Machine section. Location should be "Trusted Root Certification Authorities"
