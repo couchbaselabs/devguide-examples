d0b892fc89afed89cf980ea99c69ab23e180b88e
Ellis Breen <ellis.breen@couchbase.com>
2018-06-20 16:15:34 +0100

    PYCBC-479: add cert auth example

--- python/connecting-ssl.py
+++ python/connecting-ssl.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
 from couchbase.bucket import Bucket
+from couchbase.cluster import *
 
 # Note the `couchbases` in the scheme. This is required for SSL connections!
 cb = Bucket('couchbases://10.0.0.31/default?certpath=/tmp/couchbase-ssl-certificate.pem')
-print cb.server_nodes
+print(cb.server_nodes)
+
+cb_cluster = Cluster("http://10.0.0.31/")
+authenticator = CertAuthenticator(cert_path="/tmp/couchbase-ssl-certificate.pem", key_path="/tmp/couchbase-ssl-key.key", cluster_username="admin",
+                                  cluster_password="password")
+cb_cluster.authenticate(authenticator)
+
+cb_2 = cb_cluster.open_bucket("default")
+
+cb_2.upsert("fred", {"hello":"world"})
+
+cb_2.remove("fred")
